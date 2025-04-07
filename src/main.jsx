895be186c8d2dfc26b4288f7b52219ac78fbd454
Daniel Ribeiro da Costa <ribeirodacostadaniel57@gmail.com>
2025-04-07 16:42:39 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from "@vercel/analytics/react"
 
 import './index.css'
